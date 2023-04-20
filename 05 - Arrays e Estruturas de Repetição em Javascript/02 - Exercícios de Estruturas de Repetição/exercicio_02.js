1cd3ea830ee1d03ecd2d13a47c1223791f4e5632
Gabriel <gabriel.misato@gmail.com>
2023-04-20 13:28:02 -0300

    Exercícios de Estruturas de Repetição

---
+++ 05 - Arrays e Estruturas de Repetição em Javascript/02 - Exercícios de Estruturas de Repetição/exercicio_02.js
@@ -0,0 +1,11 @@
+// 2) Crie um programa que seja capaz de percorrer uma lista de números e imprima cada número Par encontrado
+
+
+const lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < lista.length; i++) {
+    if (i % 2 === 0) {
+        console.log(i + ' é par');
+    }
+}
+
